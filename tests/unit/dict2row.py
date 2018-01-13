4c6c13823d95a9e676cbe796a0bc48e3f5ad6d2c
Yurii Rabeshko <george.rabeshko@gmail.com>
2018-01-13 05:02:02 +0200

    Make some tests that cover the dict2row method

--- tests/unit/dict2row.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tests.main import db
-
-
-class Dict2RowTest(unittest.TestCase):
-    def setUp(self):
-        db.session.close()
-        db.drop_all()
-        db.create_all()
---
+++ tests/unit/test_dict2row.py
@@ -0,0 +1,54 @@
+from sqlalchemy_dst import dict2row, row2dict
+from tests.schema import Permission, Role, User
+from tests.test import TestCase
+
+
+class Dict2RowTest(TestCase):
+    @property
+    def user_dict(self):
+        return row2dict(self.user, depth=3, exclude=['roles', 'users'])
+
+    def test_default(self):
+        user_row = dict2row(self.user_dict, User)
+        self.assertIsInstance(user_row, User)
+        self.assertEqual(user_row.username, self.user.username)
+        self.assertEqual(user_row.password, self.user.password)
+
+    def test_rel(self):
+        user_row = dict2row(self.user_dict, User, rel={
+            'role': Role,
+            'permissions': Permission
+        })
+        self.assertEqual(len(user_row.role.permissions),
+                         len(self.user.role.permissions))
+
+    def test_exclude(self):
+        # in
+        user_row1 = dict2row(self.user_dict, User)
+        self.assertIsNotNone(user_row1.username)
+        # not in
+        user_row2 = dict2row(self.user_dict, User, exclude=['username'])
+        self.assertIsNone(user_row2.username)
+
+    def test_exclude_pk(self):
+        # in
+        user_row1 = dict2row(self.user_dict, User)
+        self.assertIsNotNone(user_row1.role_id)
+        # not in
+        user_row2 = dict2row(self.user_dict, User, exclude_pk=True)
+        self.assertIsNone(user_row2.role_id)
+
+    def test_exclude_underscore(self):
+        # in
+        user_row1 = dict2row(self.user_dict, User)
+        self.assertIsNotNone(user_row1._secret)
+        # not in
+        user_row2 = dict2row(self.user_dict, User, exclude=['password'],
+                             exclude_underscore=True)
+        self.assertIsNone(user_row2._secret)
+
+    def test_empty(self):
+        with self.assertRaises(AttributeError):
+            dict2row(self.user_dict, object)
+        with self.assertRaises(TypeError):
+            dict2row([], User)
